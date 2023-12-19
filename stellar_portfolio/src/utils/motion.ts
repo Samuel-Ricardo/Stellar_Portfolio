ef2eb3971977d69b14dd6afc83e551ae35a3ec08
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-19 16:10:25 -0300

    [ :sparkles: ] | create: util - motion > move top (animation)

--- stellar_portfolio/src/utils/motion.ts
+++ stellar_portfolio/src/utils/motion.ts
@@ -19,3 +19,15 @@ export function sideInFromRight(delay: number) {
     },
   };
 }
+
+export const slideInFromTop = {
+  hidden: { y: -100, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+    },
+  },
+};

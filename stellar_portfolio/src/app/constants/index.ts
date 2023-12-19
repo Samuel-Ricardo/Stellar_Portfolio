e29e70f481d05e4d752efde8149d93f9db9ecbe9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-19 14:55:07 -0300

    [ :gear: ] | setup: constants - skils (app)

--- stellar_portfolio/src/app/constants/index.ts
+++ stellar_portfolio/src/app/constants/index.ts
@@ -93,3 +93,66 @@ export const Socials = [
     src: '/discord.svg',
   },
 ];
+
+export const Frontend_skill = [
+  {
+    skill_name: 'Html 5',
+    Image: '/html.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Css',
+    Image: '/css.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Java Script',
+    Image: '/js.png',
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: 'Tailwind Css',
+    Image: '/tailwind.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Material UI',
+    Image: '/mui.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'React',
+    Image: '/react.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Redux',
+    Image: '/redux.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'React Query',
+    Image: '/reactquery.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Type Script',
+    Image: '/ts.png',
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: 'Next js 13',
+    Image: '/next.png',
+    width: 80,
+    height: 80,
+  },
+];

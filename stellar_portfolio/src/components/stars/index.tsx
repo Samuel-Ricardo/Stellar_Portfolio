b0a9e943e4afb22ca0454f8fddd5ec5710c74acd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-19 17:25:13 -0300

    [ :art: ] | create: component - stars  (app)

---
+++ stellar_portfolio/src/components/stars/index.tsx
@@ -0,0 +1,13 @@
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { StarBackground } from './background.component';
+
+export const Stars = () => (
+  <div className="w-full h-auto fixed inset-0 z-[20]">
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+);

9e1176713d6649558f4e896b4a17dd4b9c9dc072
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-19 17:21:46 -0300

    [ :art: ] | create: component - stars > background (app)

--- stellar_portfolio/src/components/stars/background.component.tsx
+++ stellar_portfolio/src/components/stars/background.component.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from 'react';
 //@ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm';
 import { useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
 
 const StarBackground = (props: any) => {
   const ref = useRef({ rotation: { x: 0, y: 0 } });
@@ -15,4 +16,18 @@ const StarBackground = (props: any) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="$fff"
+          size={0.002}
+          sizeAttenuation={true}
+          dethWrite={true}
+        />
+      </Points>
+    </group>
+  );
 };

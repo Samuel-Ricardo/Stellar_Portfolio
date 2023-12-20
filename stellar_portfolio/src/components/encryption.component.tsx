ed2fb004661423470254872b0d8f50bde0e2dcfe
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-20 16:30:09 -0300

    [ :art: ] | update: component - encryption (app)

--- stellar_portfolio/src/components/encryption.component.tsx
+++ stellar_portfolio/src/components/encryption.component.tsx
@@ -1,10 +1,9 @@
 import { slideInFromTop } from '@/utils/motion';
-import { MotionDiv } from './motion/div.component';
 import { MotionP } from './motion/p.component';
 import Image from 'next/image';
 
 export const Encryption = () => (
-  <div className="flex flex-row relative items-center justfy-center min-h-screen w-full h-full">
+  <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
     <div className="absolute w-auto h-auto top-0 z-[5]">
       <MotionP
         variants={slideInFromTop}

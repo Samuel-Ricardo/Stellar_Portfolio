92d783a858844446a86f40e41a034c4cb8113941
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-20 00:48:20 -0300

    [ :art: ] | create: component - Skills > other (app)

--- stellar_portfolio/src/components/skills/main.component.tsx
+++ stellar_portfolio/src/components/skills/main.component.tsx
@@ -2,6 +2,7 @@ import {
   Backend_skill,
   Frontend_skill,
   Full_stack,
+  Other_skill,
   skill_data,
 } from '@/constants';
 import { SkillText } from './text.component';
@@ -54,6 +55,18 @@ export const Skills = () => {
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Full_stack.map((image, index) => (
+          <SkillData
+            key={image.skill_name}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Other_skill.map((image, index) => (
           <SkillData
             key={image.skill_name}
             src={image.Image}

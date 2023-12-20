20b96c8623d131e78cb52076ec8030c0d62d6271
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-20 16:54:17 -0300

    [ :art: ] | update: page - home (app)

--- stellar_portfolio/src/app/page.tsx
+++ stellar_portfolio/src/app/page.tsx
@@ -1,6 +1,15 @@
+import { Encryption } from '@/components/encryption.component';
 import { Hero } from '@/components/hero/main.component';
-import Image from 'next/image';
+import { Projects } from '@/components/projects/main.component';
+import { Skills } from '@/components/skills/main.component';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <main className="h-full w-full flex flex-col gap-20">
+      <Hero />
+      <Skills />
+      <Encryption />
+      <Projects />
+    </main>
+  );
 }

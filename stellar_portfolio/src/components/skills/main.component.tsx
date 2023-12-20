0004af488adf34b6f553c24b07efe6538c1eb172
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-20 00:32:32 -0300

    [ :art: ] | create: component - Skills (app)

---
+++ stellar_portfolio/src/components/skills/main.component.tsx
@@ -0,0 +1,27 @@
+import { skill_data } from '@/constants';
+import { SkillText } from './text.component';
+import { SkillData } from './data.component';
+
+export const Skills = () => {
+  return (
+    <section
+      id="skills"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+      style={{ transform: 'scale(0.9)' }}
+    >
+      <SkillText />
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {skill_data.map((image, index) => (
+          <SkillData
+            key={image.skill_name}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};

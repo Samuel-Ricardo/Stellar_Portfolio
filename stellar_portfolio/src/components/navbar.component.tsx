e397b5bef6b734a269e54600806be81bc7ff4182
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-19 22:08:32 -0300

    [ :art: ] | create: component - navbar  (app)

---
+++ stellar_portfolio/src/components/navbar.component.tsx
@@ -0,0 +1,54 @@
+import { Socials } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const NavBar = () => {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+        <Link
+          href="#about-me"
+          className="h-auto w-auto flex flex-row items-center"
+        >
+          <Image
+            src="/NavLogo.png"
+            alt="logo"
+            width={70}
+            height={70}
+            className="cursor-pointer hover:animate-slowspin"
+          />
+
+          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
+            WebChain Dev
+          </span>
+        </Link>
+
+        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            <Link href="#about-me" className="cursor-pointer">
+              About Me
+            </Link>
+            <Link href="#skills" className="cursor-pointer">
+              Skills
+            </Link>
+            <Link href="#projects" className="cursor-pointer">
+              Projects
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-5">
+          {Socials.map((social) => (
+            <Image
+              key={social.name}
+              src={social.src}
+              alt={social.name}
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};

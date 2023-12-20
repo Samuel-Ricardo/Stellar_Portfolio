5cace7bf3a12c7b327e7125a7dcf84cd34f6e446
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-19 23:31:09 -0300

    [ :art: ] | update: black hole  (hero)

--- stellar_portfolio/src/components/hero/main.component.tsx
+++ stellar_portfolio/src/components/hero/main.component.tsx
@@ -6,7 +6,7 @@ export const Hero = () => (
       autoPlay
       muted
       loop
-      className="rotate-180 fixed top-[-340px] h-full w-full left-0 z-[1] object-cover"
+      className="rotate-180 absolute top-[-340px] w-full h-full left-0 z-[1] object-cover"
     >
       <source src="/blackhole.webm" type="video/webm" />
     </video>
